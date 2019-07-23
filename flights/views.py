7090e41663cf852910bdee1d3d0848e4c2a3dcfa
leyvai <bu50.88@gmail.com>
2019-07-23 19:02:32 +0300

    task complete

--- flights/views.py
+++ flights/views.py
@@ -1,1 +1,14 @@
 from .models import Flight, Booking
+from rest_framework.generics import ListAPIView
+from django.shortcuts import render
+from .serializer import FlightSerializer, BookingSerializer
+from datetime import *
+
+class Flights(ListAPIView):
+	queryset = Flight.objects.all()
+	serializer_class = FlightSerializer	
+
+class Bookings(ListAPIView):
+	queryset = Booking.objects.filter(date__gt = date.today())
+	serializer_class = BookingSerializer	
+		
